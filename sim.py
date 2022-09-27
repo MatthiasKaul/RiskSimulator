ad27ac544afd175a8996b911b2ec09b1b373c620
MatthiasKaul <87767629+MatthiasKaul@users.noreply.github.com>
2022-09-27 18:18:55 +0200

    Initial commit

---
+++ sim.py
@@ -0,0 +1,39 @@
+from random import randint
+
+def playRound(attackers, defenders):
+    effectiveAttackers = min(3, attackers)
+    effectiveDefenders = min(2, defenders)
+
+    atkDice = []
+    defDice = []
+
+    for _ in range(effectiveAttackers):
+        atkDice.append(randint(1,6))
+
+    for _ in range(effectiveDefenders):
+        defDice.append(randint(1,6))
+
+    atkDice.sort()
+    atkDice = atkDice[::-1]
+    defDice.sort()
+    defDice = defDice[::-1]
+
+    atkLoss = 0
+    defLoss = 0
+    for i in range(min(effectiveAttackers, effectiveDefenders)):
+        if atkDice[i] > defDice[i]:
+            defLoss += 1
+        else:
+            atkLoss += 1
+
+    return atkLoss, defLoss
+
+attackers = 1000000
+defenders = 1000000
+
+while(attackers > 0 and defenders > 0):
+    aL, dL = playRound(attackers, defenders)
+    attackers -= aL
+    defenders -= dL
+
+print(f"Attackers left: {attackers}, Defenders left: {defenders}")
