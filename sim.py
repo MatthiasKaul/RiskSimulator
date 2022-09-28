f3db42ae9a3bb0876454feb045ff314a31a9e128
Matthias Kaul <matthias.kaul@tuhh.de>
2022-09-28 14:58:31 +0200

    Built extremely good UI

--- sim.py
+++ sim.py
@@ -34,11 +34,56 @@ def playRound(attackers, defenders):
 
 
 
-attackers = int(sys.argv[1])
-defenders = int(sys.argv[2])
-while(attackers > 0 and defenders > 0):
-    aL, dL = playRound(attackers, defenders)
-    attackers -= aL
-    defenders -= dL
+def simulate(attackers, defenders):
+    while(attackers > 0 and defenders > 0):
+        aL, dL = playRound(attackers, defenders)
+        attackers -= aL
+        defenders -= dL
+    return attackers, defenders
 
-print(f"Attackers left: {attackers}, Defenders left: {defenders}")
+
+
+
+import PySimpleGUI as sg
+QT_ENTER_KEY1 = 'special 16777220'
+QT_ENTER_KEY2 = 'special 16777221'
+
+
+attackers = 1
+defenders = 1
+
+layout = [[[sg.Text('Attackers', size =(15, 1)), sg.InputText("3", key = "__ATTACKERS"), sg.Button("+", key = "__INC_ATK"), sg.Button("-", key = "__DEC_ATK")],
+            [sg.Text('Defenders', size =(15, 1)), sg.InputText("1", key = "__DEFENDERS"), sg.Button("+", key = "__INC_DEF"), sg.Button("-", key = "__DEC_DEF")]],
+            [sg.Button("Simulate!")]]
+window = sg.Window(title="Best Risk Simulator", layout=layout, margins=(200, 200), return_keyboard_events=True)
+while True:
+    event, values = window.read()
+    
+    if  event == sg.WIN_CLOSED or event == "Escape:9":
+        break
+    attackers = int(values["__ATTACKERS"])
+    defenders = int(values["__DEFENDERS"])
+
+    if event == "__INC_ATK":
+        attackers += 1
+    if event == "__DEC_ATK":
+        attackers -= 1
+
+    if event == "__INC_DEF":
+        defenders += 1
+    if event == "__DEC_DEF":
+        defenders -= 1
+
+    if event == "Simulate!":
+        attackers, defenders = simulate(attackers, defenders)
+        #print(f"{attackers} {defenders}")
+    if event in ('\r', QT_ENTER_KEY1, QT_ENTER_KEY2, "Return:36"):
+        attackers, defenders = simulate(attackers, defenders)
+
+
+    window["__ATTACKERS"].Update(attackers)
+    window['__DEFENDERS'].Update(defenders)
+
+
+
+window.close()
